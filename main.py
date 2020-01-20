01085ad159ecbf037a4344e53d1c8b18c2b8fee3
genix <intergenixyt@gmail.com>
2020-01-20 21:30:45 +0100

    add comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import funs
 import calculate
 
-funs.rename_files()
+# funs.rename_files()  # run only when images have their original names
 funs.clear_results()
 calculate.calculate()
 funs.check_correctness()
