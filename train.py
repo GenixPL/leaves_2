6320a8f11ebcfd3cf5acc52045a44c28ad49c7ff
genix <intergenixyt@gmail.com>
2020-01-20 13:59:58 +0100

    sth

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from utils.set_trainer import SetTrainer
 
 baseDirPath = os.path.dirname(__file__) + "/data/train/"
 
-for i in range(1, 2):  # TODO (7)
+for i in range(1, 7):  # TODO (7)
     setDirPath = baseDirPath + "type_" + str(i)
     SetTrainer(setDirPath).train()
 
--- utils/models/set_slimness.py
+++ utils/models/set_slimness.py
@@ -1,5 +1,7 @@
 class SetSlimness:
 
+    # while check if a given img fts those ratios we have to add some margin for error
+
     def __init__(self, smallest_ration, biggest_ration):
         self.smallest_ratio = smallest_ration
         self.biggest_ratio = biggest_ration
